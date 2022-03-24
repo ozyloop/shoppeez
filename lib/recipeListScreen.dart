b32cb388c3fd12198b184ea3a64df5f4cc7107f6
ozyloop <thomatom2014@gmail.com>
2022-03-24 16:23:43 +0200

    last version for sprint review

--- lib/recipeListScreen.dart
+++ lib/recipeListScreen.dart
@@ -329,7 +329,46 @@ Row(mainAxisAlignment: MainAxisAlignment.center,
                         style: TextButton.styleFrom(primary: Colors.black, backgroundColor: Colors.orange, fixedSize: Size(60,55), minimumSize: Size(50,40)),
 
 
-                        onPressed: () {},
+                        onPressed: () {
+
+                          SearchBar(context, _controller).then((result) {
+                            print('In Builder');
+                          });
+
+                          /*TextField(
+                            controller: _controller,
+                            onSubmitted: (String value) async
+                            {
+                              await showDialog<void>(
+                                context: context,
+                                builder: (BuildContext context)
+                                {
+                                  return AlertDialog(
+                                    title: const Text('Thanks!'),
+                                    content: Text('You typed "$value", which has length ${value.characters.length}.'),
+                                    actions: <Widget>[
+                                    ],
+                                  );
+                                },
+                              );
+                            },
+                            decoration: InputDecoration(
+                              hintText: 'ingredients',
+                              labelText: 'What do you have to cook ? ',
+                              prefixIcon: Icon(Icons.food_bank, color: Colors.red),
+                              // icon: Icon(Icons.food),
+                              suffixIcon: _controller.text.isEmpty
+                                  ? Container(width: 0) :
+                              IconButton(
+                                icon: Icon(Icons.close),
+                                onPressed: () =>  _controller.text ="",
+                              ),
+                              border: OutlineInputBorder(),
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            textInputAction: TextInputAction.done,
+                          );*/
+                        },
                         child: const Icon(Icons.search, size:30 ),
 
                       ),
@@ -612,3 +651,36 @@ class ShopItemWidget extends StatelessWidget
 }
 
 
+Future<Widget> SearchBar(context, _controller) async => TextField(
+    controller: _controller,
+    onSubmitted: (String value) async
+    {
+      await showDialog<void>(
+        context: context,
+        builder: (BuildContext context)
+        {
+          return AlertDialog(
+            title: const Text('Thanks!'),
+            content: Text('You typed "$value", which has length ${value.characters.length}.'),
+            actions: <Widget>[
+            ],
+          );
+        },
+      );
+    },
+    decoration: InputDecoration(
+      hintText: 'ingredients',
+      labelText: 'What do you have to cook ? ',
+      prefixIcon: Icon(Icons.food_bank, color: Colors.red),
+      // icon: Icon(Icons.food),
+      suffixIcon: _controller.text.isEmpty
+          ? Container(width: 0) :
+      IconButton(
+        icon: Icon(Icons.close),
+        onPressed: () =>  _controller.text ="",
+      ),
+      border: OutlineInputBorder(),
+    ),
+    keyboardType: TextInputType.emailAddress,
+    textInputAction: TextInputAction.done,
+  );
